11ba14b1b6c40ca67671b5d83971acaec3722ec8
chengi i <1078994941@qq.com>
2022-12-13 04:08:07 +0000

    fix: initial

--- test/src/main.js
+++ test/src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import App from './App.vue'
+import router from './router'
+import { usePermissStore } from './store/permiss'
+import 'element-plus/dist/index.css'
+import './assets/css/icon.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(ElementPlus, {
+    locale: zhCn,
+})
+// 注册elementplus图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+
+app.mount('#app')
